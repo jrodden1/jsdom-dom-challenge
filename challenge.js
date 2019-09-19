97436a2165421408b08c94f80f940dbad9dabb06
Jeremiah Rodden <jrodden1@gmail.com>
2019-09-19 16:16:49 -0600

    refactored code and added comments for readability

--- challenge.js
+++ challenge.js
@@ -1,5 +1,5 @@
+//Variable Declarations
 let count = 0 
-
 const counter = document.querySelector("#counter")
 const pauseButton = document.querySelector("#pause")
 const plusButton = document.querySelector("#plus")
@@ -10,52 +10,39 @@ const commentInputText = document.querySelector("#comment")
 const commentsList = document.querySelector("#list")
 const likesList = document.querySelector(".likes")
 
-function timerIncrement() {
-   count++
-   counter.innerText = count
-   console.log(count)
-}
-
-function timerDecrement() {
-   count--
-   counter.innerText = count
-   console.log(count)
-}
-
 let likedNumbers = []
 
-function addHeartedNumberToList() {
-   function addLike() {
-      const likedNum = captureHeartedNumber()
-      const foundNumber = likedNumbers.find(obj => obj.number == likedNum)
+//Function Declarations
 
-      if(foundNumber) {
-         //increment that object's "times" value 
-         foundNumber.times++
-         existingLi = document.getElementById(`${foundNumber.number}`)
-         existingLi.textContent = `${foundNumber.number} liked ${foundNumber.times} times`
-      } else {
-   //    else, if I don't find the number then add a new object to the array with the number and times attribs
-         const newLikedNum = {
-            number: likedNum,
-            times: 1
-         }
-         likedNumbers.push(newLikedNum)
-         
-         const newListItem = createHeartedNumberListItem()
-         newListItem.textContent = `${newLikedNum.number} liked ${newLikedNum.times} time`
-         newListItem.id = `${newLikedNum.number}`
-         likesList.appendChild(newListItem)
-      }
-   }   
-
+//Liking a Number Functions
+function addHeartedNumberToList() {   
    addLike()
-
 }
 
+function addLike() {
+   const likedNum = captureHeartedNumber()
+   const foundNumber = likedNumbers.find(obj => obj.number == likedNum)
 
-
-
+   if(foundNumber) {
+      //increment that object's "times" value if it is found in my likedNumbers array
+      foundNumber.times++
+      existingLi = document.getElementById(`${foundNumber.number}`)
+      existingLi.textContent = `${foundNumber.number} liked ${foundNumber.times} times`
+   } else {
+      // else, if I don't find the number then add a new object to the likedNumbers array with the number and times attribs
+      const newLikedNum = {
+         number: likedNum,
+         times: 1
+      }
+      likedNumbers.push(newLikedNum)
+      
+      //Go ahead and build out the new list item and post it the screen
+      const newListItem = createHeartedNumberListItem()
+      newListItem.textContent = `${newLikedNum.number} liked ${newLikedNum.times} time`
+      newListItem.id = `${newLikedNum.number}`
+      likesList.appendChild(newListItem)
+   }
+}
 
 function captureHeartedNumber() {
    return count 
@@ -65,6 +52,8 @@ function createHeartedNumberListItem() {
    let li = document.createElement("li")
    return li
 }
+
+//Adding comments functions
 
 function addComment(event) {
    event.preventDefault()
@@ -77,24 +66,36 @@ function addComment(event) {
    commentInputText.value = ""
 }
 
+//Incrementing/decrementing the timer
+function timerIncrement() {
+   count++
+   counter.innerText = count
+   console.log(count)
+}
 
+function timerDecrement() {
+   count--
+   counter.innerText = count
+   console.log(count)
+}
 
 document.addEventListener("DOMContentLoaded", function() {
+   //Start the initial timer 
    let timerId = setInterval(timerIncrement, 1000)
+   
+   //adding event listeners to my buttons
    pauseButton.addEventListener("click", pauseAll)
    plusButton.addEventListener("click", timerIncrement)
    minusButton.addEventListener("click", timerDecrement)
    heartButton.addEventListener("click", addHeartedNumberToList)
    commentForm.addEventListener("submit", addComment)
 
+   //setting status of my paused variable to false to start with 
    let paused = false
 
+   //Functions to enable Pausing of all actions -- minus the comment function.  
+   //As I read the specs, I didn't see why it should include the comment button.
    function pauseAll() {
-      /* Requirements still need to be added:
-         - disable all the other buttons (i think I can add an attribute of disabled to the button)
-         - change pause button text to "resume"
-         - bonus - dim everything?? 
-      */
       if (paused == false) {
          clearInterval(timerId)
          paused = true
@@ -124,8 +125,3 @@ document.addEventListener("DOMContentLoaded", function() {
       heartButton.disabled = false
    }
 });
-
-
-
-
-
